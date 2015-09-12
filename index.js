fdffed90f57133c54fc98d59687d388fbffd1333
github@umbrella.zone <github@phil.today>
2015-09-12 19:36:38 +0000

    interim update

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 /// <reference path="typings/tsd.d.ts" />
 var path, through;
-
 through = require("through2");
 path = require("path");
-
-module.exports = (options,mojo = undefined) => {
+module.exports = function (options, mojo) {
     /* -------------------------------------------------------------------------
     ------------------------- helper functions ----------------------------------
     --------------------------------------------------------------------------
     */
-    
-    
-    
+    if (mojo === void 0) { mojo = undefined; }
     if (mojo != undefined) {
         mojo.log("now prepocessing blog");
-    } else {
-        console.log('you do not seem to use mojo.io');
-        mojo = {}
-        mojo.log = function(logthis) {
-            console.log(logthis);
-        }
     }
-
+    else {
+        console.log('you do not seem to use mojo.io');
+        mojo = {};
+        mojo.log = function (logthis) {
+            console.log(logthis);
+        };
+    }
     /*--------------------------------------------------------------------------
     ---------------------- returned stream --------------------------------------
     --------------------------------------------------------------------------
     */
-
-    return through.obj(function(file, enc, cb) {
+    return through.obj(function (file, enc, cb) {
         if (file.isNull() === true) {
             cb(null, file);
             return;
@@ -36,16 +31,11 @@ module.exports = (options,mojo = undefined) => {
             mojo.log("streaming not supported");
             return;
         }
-        
         //append legal.json from file.content to file.data
         jsonString = String(file.contents);
         //create file.data in case it doesn't exist
         file.data = file.data | {};
         file.data.legal = JSON.parse(jsonString);
-        
-        
         //get the imprint jade file from umbrella.zone and append it to file.content
-        
-        
     });
 };
