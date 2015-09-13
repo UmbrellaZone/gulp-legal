63b7f83f5d3d8fa82e4a6b357fd5337d43817c69
github@umbrella.zone <github@phil.today>
2015-09-13 15:36:17 +0000

    fixed through2 dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /// <reference path="typings/tsd.d.ts" />
 var path, through;
+var https = require("https");
 through = require("through2");
 path = require("path");
 module.exports = function (options, mojo) {
@@ -31,11 +32,43 @@ module.exports = function (options, mojo) {
             mojo.log("streaming not supported");
             return;
         }
+        /* ------------------------------------------------------------------------------------
+       ------------ create umbrella object ----------------------------------------------------
+       ------------------------------------------------------------------------------------ */
+        var umbrella = {};
+        //get the jade Template from GitHub
+        var request = require('request');
+        request.get('https://raw.githubusercontent.com/UmbrellaZone/umbrella-legal/master/00dev/jade/index.jade', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                umbrella.jadeTemplate = body;
+            }
+            else {
+                console.log('could not get jade template for the umbrella.zone imprint');
+            }
+            ;
+        });
+        //get the legal.json file
+        request.get('https://raw.githubusercontent.com/UmbrellaZone/umbrella-legal/master/01build/content.json', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                umbrella.legalJson = body;
+            }
+            else {
+                console.log('could not get jade template for the umbrella.zone imprint');
+            }
+            ;
+        });
         //append legal.json from file.content to file.data
-        jsonString = String(file.contents);
-        //create file.data in case it doesn't exist
+        umbrella.contactJson = String(file.contents);
+        /* ------------------------------------------------------------------------------------
+        ------------ build the file.data object -----------------------------------------------
+        ------------------------------------------------------------------------------------ */
+        // create the file.data object in case it does not yet exist
         file.data = file.data | {};
-        file.data.legal = JSON.parse(jsonString);
-        //get the imprint jade file from umbrella.zone and append it to file.content
+        // add legal object to file.data
+        file.data.legal = {};
+        //parse the legalContactJsonString to the file.data.legal object
+        file.data.legal.contact = JSON.parse(umbrella.contactJson);
+        //replace file.content with the jade template file
+        file.contents = new Buffer(umbrella.jadeTemplate);
     });
 };
